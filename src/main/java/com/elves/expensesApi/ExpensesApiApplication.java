d8eec59520e1f680a1edf050cab730cba22df0fb
Elves Aguiar <elvesmhs@hotmail.com>
2023-12-21 12:01:50 -0300

    initial commit

---
+++ src/main/java/com/elves/expensesApi/ExpensesApiApplication.java
@@ -0,0 +1,13 @@
+package com.elves.expensesApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elves/expensesApi/ExpensesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elves.expensesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
