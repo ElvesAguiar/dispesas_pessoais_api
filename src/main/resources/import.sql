874c86a11a7c1c7ffec2acacfb81066e6d089e60
Elves Aguiar <elvesmhs@hotmail.com>
2023-12-22 17:53:52 -0300

    make it better

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 -- INSERT INTO tb_transaction  (title,transaction_value,trasaction_date) VALUES ('cartao', 40.9, CURRENT_TIMESTAMP() );
 -- INSERT INTO tb_transaction  (title,transaction_value,trasaction_date) VALUES ('pedra', 0.39, CURRENT_TIMESTAMP() );
--- INSERT INTO tb_transaction  (title,transaction_value,trasaction_date) VALUES ('maca', 1.9, DATE '2023-12-19' );
+INSERT INTO tb_transaction  (title,transaction_value,trasaction_date) VALUES ('maca', 1.9, DATE '2023-12-19' );
 --
